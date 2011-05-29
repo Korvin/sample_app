18f73b64b5c238e071ea11f85cb08645ee3a739b
Stepan Prokipchyn <korvin@Matrix.(none)>
2011-05-29 20:53:52 +0300

    Layout finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
+  match "/signup",  :to => "users#new"
   match "/about",   :to => "pages#about"
   match "/contact", :to => "pages#contact"
   match "/help",    :to => "pages#help"
